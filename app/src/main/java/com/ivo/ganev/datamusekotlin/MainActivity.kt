d01ed3426dff013e791e60e37689189a8954299d
Ivo-Vladimirov-Ganev <ivo.ganev@yahoo.com>
2020-12-09 12:27:37 +0000

    Add Kotlin coroutines support

---
+++ app/src/main/java/com/ivo/ganev/datamusekotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ivo.ganev.datamusekotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
