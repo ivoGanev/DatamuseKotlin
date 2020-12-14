284bce780f4345bcdb9734c1ccdf155168a5a236
Ivo-Vladimirov-Ganev <ivo.ganev@yahoo.com>
2020-12-14 13:58:15 +0000

    Add hand-written composite serializer for WordRequest

--- app/src/main/java/com/ivo/ganev/datamusekotlin/MainActivity.kt
+++ app/src/main/java/com/ivo/ganev/datamusekotlin/MainActivity.kt
@@ -3,8 +3,12 @@ package com.ivo.ganev.datamusekotlin
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ivo.ganev.datamusekotlin.api.DatamuseOkHttpClientGet
+import com.ivo.ganev.datamusekotlin.internal.WordRequest
+import com.ivo.ganev.datamusekotlin.internal.Words
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +18,13 @@ class MainActivity : AppCompatActivity() {
 
         GlobalScope.launch {
             val get = client.get("https://api.datamuse.com/words?sp=hipopatamus")
-            println(get)
+            get.fold({}, {
+              //  val o = Json.parseToJsonElement(it)
+                val o = "{\"word\":\"hippopotamus\",\"score\":501,\"defs\":[\"n\\tmassive thick-skinned herbivorous animal living in or around rivers of tropical Africa\"]},{\"word\":\"hippotamus\",\"score\":1}"
+                val decodeFromJsonElement = Json.decodeFromString<WordRequest>(o)
+
+                println(decodeFromJsonElement)
+            })
         }
     }
 }
---
+++ app/src/test/java/com/ivo/ganev/datamusekotlin/internal/ObjectMapperTest.kt
@@ -0,0 +1,11 @@
+package com.ivo.ganev.datamusekotlin.internal
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Test
+
+class ObjectMapperTest {
+    @Test
+    fun `test mapper`() {
+    }
+}
