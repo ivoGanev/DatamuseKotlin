07db603b4584793a47ed907532298735bd542202
Ivo-Vladimirov-Ganev <ivo.ganev@yahoo.com>
2020-12-31 14:02:07 +0000

    Enable printing the Json result in app

--- app/src/main/java/com/ivo/ganev/datamusekotlin/DatamuseActivity.kt
+++ app/src/main/java/com/ivo/ganev/datamusekotlin/DatamuseActivity.kt
@@ -9,11 +9,15 @@ import androidx.appcompat.app.AppCompatActivity
 import com.ivo.ganev.datamusekotlin.UiConstraintElement.*
 import com.ivo.ganev.datamusekotlin.api.HardConstraint
 import com.ivo.ganev.datamusekotlin.api.HardConstraint.RelatedWords.Code.APPROXIMATE_RHYMES
-import com.ivo.ganev.datamusekotlin.core.WordResponse.Element.Word
+import com.ivo.ganev.datamusekotlin.api.MetadataFlag
+import com.ivo.ganev.datamusekotlin.api.Topic
+import com.ivo.ganev.datamusekotlin.core.WordResponse
+import com.ivo.ganev.datamusekotlin.core.WordResponse.Element.*
 
 import com.ivo.ganev.datamusekotlin.databinding.DatamuseDemoActivityBinding
 import com.ivo.ganev.datamusekotlin.extenstions.isWithId
 import com.ivo.ganev.datamusekotlin.extenstions.string
+import java.util.*
 
 
 class DatamuseActivity : AppCompatActivity(),
@@ -44,7 +48,13 @@ class DatamuseActivity : AppCompatActivity(),
 
             it.forEach { wordResponse ->
                 val word = wordResponse[Word::class]?.word
-                binding.tvResponse.append("$i. $word \n")
+                val score = wordResponse[Score::class]?.score
+                val definitions  = wordResponse[Definitions::class]?.defs
+                val tags = wordResponse[Tags::class]?.tags
+                val syllablesCount = wordResponse[SyllablesCount::class]?.numSyllables
+                val defHeadwords = wordResponse[DefHeadwords::class]?.defHeadword
+
+                binding.tvResponse.append("$i. $word , Score: $score, Definitions: $definitions, Tags: $tags, Syllable Count: $syllablesCount, Def Headwords: $defHeadwords\n")
                 i++
             } })
     }
@@ -56,6 +66,13 @@ class DatamuseActivity : AppCompatActivity(),
     }
 
     class ApiData(private val binding: DatamuseDemoActivityBinding) : DatamuseActivityData() {
+
+        private val metadataCheckbox = mapOf(binding.cbDefinitions to MetadataFlag.DEFINITIONS,
+                binding.cbPartsOfSpeech to MetadataFlag.PARTS_OF_SPEECH,
+                binding.cbPronunciation to MetadataFlag.PRONUNCIATIONS,
+                binding.cbSyllableCount to MetadataFlag.SYLLABLE_COUNT,
+                binding.cbWordFrequency to MetadataFlag.WORD_FREQUENCY)
+
         override fun getConstraint(): HardConstraint {
             val constraintElement = binding.constraintSpinner.selectedItem as UiConstraintElement
             val keyword = binding.etWord.string()
@@ -64,6 +81,29 @@ class DatamuseActivity : AppCompatActivity(),
                 is RelatedWordsElement -> constraintElement.create(APPROXIMATE_RHYMES, keyword)
                 else -> constraintElement.create(keyword)
             }
+        }
+
+        override fun getTopics(): String {
+            return binding.etTopics.string()
+        }
+
+        override fun getLeftContext(): String {
+            return binding.etLc.string()
+        }
+
+        override fun getRightContext(): String {
+            return binding.etRc.string()
+        }
+
+        override fun getMaxResults(): Int {
+            return Integer.parseInt(binding.etMaxResults.string())
+        }
+
+        override fun getMetadata(): EnumSet<MetadataFlag>? {
+            val flags = metadataCheckbox.filter { it.key.isChecked }.values
+            val set = EnumSet.noneOf(MetadataFlag::class.java)
+            set.addAll(flags)
+            return set
         }
     }
 }
--- app/src/main/java/com/ivo/ganev/datamusekotlin/DatamuseActivityView.kt
+++ app/src/main/java/com/ivo/ganev/datamusekotlin/DatamuseActivityView.kt
@@ -14,25 +14,15 @@ abstract class DatamuseActivityData()  {
      * */
     abstract fun getConstraint(): HardConstraint
 
-    protected open fun getTopics(): String? {
-        return null
-    }
+    protected open fun getTopics(): String = ""
 
-    protected open fun getLeftContext(): String? {
-        return null
-    }
+    protected open fun getLeftContext(): String = ""
 
-    protected open fun getRightContext(): String? {
-        return null
-    }
+    protected open fun getRightContext(): String = ""
 
-    protected open fun getMaxResults(): Int? {
-        return null
-    }
+    protected open fun getMaxResults(): Int = 100
 
-    protected open fun getMetadata(): EnumSet<MetadataFlag>? {
-        return null
-    }
+    protected open fun getMetadata(): EnumSet<MetadataFlag>?  = null
 
     fun toUrlString(): String {
         return buildWordsEndpointUrl {
